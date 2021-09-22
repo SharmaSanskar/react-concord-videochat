6584de65cad053994b5c17eb05300be15aee2fe8
Sanskar Sharma <sharmasanskar004@gmail.com>
2021-09-22 17:25:56 +0530

    Completed room call

--- server.js
+++ server.js
@@ -12,15 +12,13 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+const allRooms = {};
+
 io.on("connection", (socket) => {
   socket.on("join", ({ userId }) => {
     socket.join(userId);
-    console.log("Joined id:" + userId);
+    // console.log("Joined id:" + userId);
   });
-
-  // socket.on("disconnect", () => {
-  //   socket.broadcast.emit("endCall");
-  // });
 
   socket.on("callEnded", ({ otherUser }) => {
     io.to(otherUser).emit("endCall");
@@ -40,6 +38,45 @@ io.on("connection", (socket) => {
 
   socket.on("answerCall", ({ callingUser, signalData }) => {
     io.to(callingUser).emit("callAccepted", signalData);
+  });
+
+  // Room call
+  socket.on("join room", ({ roomId, memberId }) => {
+    if (allRooms[roomId]) {
+      allRooms[roomId].push(memberId);
+    } else {
+      allRooms[roomId] = [memberId];
+    }
+    const membersInRoom = allRooms[roomId].filter((id) => id !== memberId);
+    socket.emit("all members", membersInRoom);
+  });
+
+  socket.on(
+    "sending signal to member",
+    ({ memberToSignal, newMemberId, signalData }) => {
+      io.to(memberToSignal).emit("new member", { signalData, newMemberId });
+    }
+  );
+
+  socket.on(
+    "returning to new member",
+    ({ signalData, newMemberId, memberId }) => {
+      io.to(newMemberId).emit("receiving returned signal", {
+        signalData,
+        memberId,
+      });
+    }
+  );
+
+  socket.on("invite", ({ memberToCall, roomId, roomName }) => {
+    io.to(memberToCall).emit("room call incoming", { roomId, roomName });
+  });
+
+  socket.on("room call ended", (roomId) => {
+    allRooms[roomId]?.forEach((id) => {
+      io.to(id).emit("room call over");
+    });
+    delete allRooms[roomId];
   });
 });
 
